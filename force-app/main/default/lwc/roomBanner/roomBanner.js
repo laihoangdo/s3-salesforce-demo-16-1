fb25c2b1924d801254b42144abcbda9aabd7aee6
duy.huynh <duy.huynh@s3corp.com.vn>
2024-01-19 16:48:05 +0700

    Room: Create some new components in room's page hotel

---
+++ force-app/main/default/lwc/roomBanner/roomBanner.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/RoomImages'
+export default class BannerRoom extends LightningElement {
+    imageUrl = IMAGES + '/hotel-c.jpg';
+}
---
+++ force-app/main/default/lwc/roomPrivateDining/roomPrivateDining.js
@@ -0,0 +1,40 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/RoomImages'
+export default class SectionRoom1 extends LightningElement {
+    imageUrl1 = IMAGES + '/karuizawaA-56_Shelf1-2048x1366.jpg'
+    imageUrl2 = IMAGES + '/karuizawaB-16_Wall1-2048x1366.jpg'
+    imageUrl3 = IMAGES + '/hotel-c.jpg'
+
+    renderedCallback() {
+        const listImagae = this.template.querySelectorAll(".child_image");
+        const leftButton = this.template.querySelector(".swiper-btn-left");
+        const rightButton = this.template.querySelector(".swiper-btn-right");
+        const contentImage = this.template.querySelector(".right__content-img");
+        const listAllDotted = this.template.querySelectorAll(".dotted");
+        let indx = 0;
+
+        const styleDottedActive = (indx) => {
+            listAllDotted.forEach((item, index) => {
+                indx === index
+                    ? item.classList.add("active")
+                    : item.classList.remove("active");
+            });
+        };
+
+        leftButton.addEventListener("click", () => {
+            if (indx > 0) {
+                indx -= 1
+            } else {
+                indx = listImagae.length - 1
+            }
+            styleDottedActive(indx)
+            contentImage.style.transform = `translate3d(${indx * (-1 * 100)}%, 0, 0)`;
+        });
+        rightButton.addEventListener("click", () => {
+            indx = indx < listImagae.length - 1 ? indx + 1 : 0;
+            styleDottedActive(indx)
+            contentImage.style.transform = `translate3d(${indx * (-1 * 100)}%, 0, 0)`;
+        });
+
+    }
+}
